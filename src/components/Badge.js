887b077d08a77284fe50b03eacae0647343e7e23
dzetinoc <65565072+dzetinoc@users.noreply.github.com>
2020-07-28 20:31:05 -0600

    1

---
+++ src/components/Badge.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './styles/Badge.css'
+import confLogo from '../images/Logo Platzi.jpg';
+class Badge extends React.Component {
+    render(){
+        return (
+        <div className="Badge">
+            <div className="Badge_header">
+                <img src={confLogo} alt="Logo de la Conferencia"></img>
+            </div>
+
+            <div>
+            <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+                <h1>Dolores <br/> Zetino</h1>
+            </div>               
+            
+            <div>
+                <p>Frontend Engineer</p>
+                <p>@dzetino</p>
+            </div>
+
+            <div>
+                #platziconf
+            </div>
+
+         </div>
+            
+        )        
+    }    
+}
+       
+
+
+export default Badge;
