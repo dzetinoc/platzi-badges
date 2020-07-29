8c8075f934fe0683b3781ad49ac58717f34cb1ed
dzetinoc <65565072+dzetinoc@users.noreply.github.com>
2020-07-29 08:11:41 -0600

    prueba

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -6,26 +6,21 @@ class Badge extends React.Component {
     render(){
         return (
         <div className="Badge">
-            <div className="Badge_header">
+            <div>
                 <img src={confLogo} alt="Logo de la Conferencia"></img>
             </div>
-
             <div>
             <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
                 <h1>Dolores <br/> Zetino</h1>
-            </div>               
-            
+            </div>                           
             <div>
                 <p>Frontend Engineer</p>
                 <p>@dzetino</p>
             </div>
-
             <div>
                 #platziconf
             </div>
-
-         </div>
-            
+         </div>           
         )        
     }    
 }
